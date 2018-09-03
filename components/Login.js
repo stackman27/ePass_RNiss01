dc2fcd935a69efebb39f5516cd69d486fb6e5591
stackman27 <sishirg27@gmail.com>
2018-09-03 15:36:59 -0700

    Basic LoginScreen Setup

--- components/Login.js
+++ components/Login.js
@@ -1,14 +1,58 @@
 import React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
- 
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+import Form from './Form';
+import Logo from './Logo';
+
 export class Login extends React.Component { 
   
+
+    _gotoSignup(){
+        alert('Signup');
+    }
+
   render(){
     return (
-      <View >    
-          <Text> Login Screen </Text>
+      <View style = {styles.container}>
+            <Logo />
+            
+             <Form type="Login"/>
+
+             <View style = {styles.signupTextCont}> 
+                  <Text style = {styles.signupText}> Don't have an account yet? </Text>
+                    <TouchableOpacity onPress = {this._gotoSignup}> 
+                        <Text style = {styles.signupButton}> SignUp </Text>
+                    </TouchableOpacity> 
+             </View>     
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#8E3A9D',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    signupTextCont: {
+        flexGrow: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        paddingVertical: 16,
+        flexDirection: 'row'
+    },
+
+    signupText: {
+        color: 'rgba(255, 255, 255, 0.6)',
+        fontSize: 16
+    },
+    signupButton: {
+        color: '#ffffff',
+        fontSize: 16, 
+        fontWeight: '500'
+    }
+});
  
